6a846d533a526d8ed6df2765c1e352c4acb06414
nezuedu <qloudpatch@hotmail.com>
2026-06-25 21:40:15 +0200

    Added the function file

---
+++ word_statistics.py
@@ -0,0 +1,15 @@
+def word_statistics(string: str) -> dict:
+    """
+    Function which can determine how many times a word is seen in the same string value.
+    :param string:Any input text.
+    :return: Dictionary which contains words from the input text as keys and their count inside the text as their values
+    """
+
+    string = string.lower()
+    dic = {}
+    for word in string.split():
+        if word in dic:
+            dic[word] += 1
+        else:
+            dic[word] = 1
+    return dic
